44b91cf408149ec17cc6f746659f5daf8a7c9140
Naman Agarwal <agarwalnaman35@gmail.com>
2022-05-06 22:54:34 +0530

    Add styling in welcome page

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -21,3 +21,16 @@ export default function Welcome() {
   );
 }
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #65ee48;
+  }
+`;
