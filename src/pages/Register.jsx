6b2c107df5039d274b72ae0f8718baa385cfba6b
Naman Agarwal <agarwalnaman35@gmail.com>
2022-05-06 22:51:44 +0530

    validation in register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -33,7 +33,33 @@ export default function Register() {
     setValues({ ...values, [event.target.name]: event.target.value });
   };
 
+  const handleValidation = () => {
+    const { password, confirmPassword, username, email } = values;
+    if (password !== confirmPassword) {
+      toast.error(
+        "Password and confirm password should be same.",
+        toastOptions
+      );
+      return false;
+    } else if (username.length < 3) {
+      toast.error(
+        "Username should be greater than 3 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (password.length < 8) {
+      toast.error(
+        "Password should be equal or greater than 8 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (email === "") {
+      toast.error("Email is required.", toastOptions);
+      return false;
+    }
 
+    return true;
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
