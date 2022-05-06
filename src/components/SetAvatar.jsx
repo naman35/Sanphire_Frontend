f34326550028e8b0851be4ee9f2746e8f1702f13
Naman Agarwal <agarwalnaman35@gmail.com>
2022-05-06 22:57:27 +0530

    Styling of avatar page

--- src/components/SetAvatar.jsx
+++ src/components/SetAvatar.jsx
@@ -78,3 +78,58 @@ export default function SetAvatar() {
   );
 }
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 3rem;
+  
+  height: 100vh;
+  width: 100vw;
+
+  .loader {
+    max-inline-size: 100%;
+  }
+
+  .title-container {
+    h1 {
+      color: white;
+    }
+  }
+  .avatars {
+    display: flex;
+    gap: 2rem;
+
+    .avatar {
+      border: 0.4rem solid transparent;
+      padding: 0.4rem;
+      border-radius: 5rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      transition: 0.5s ease-in-out;
+      img {
+        height: 6rem;
+        transition: 0.5s ease-in-out;
+      }
+    }
+    .selected {
+      border: 0.4rem solid #477a40;
+    }
+  }
+  .submit-btn {
+    background-color: #65ee48;
+    color: black;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #477a40;
+    }
+  }
+`;
