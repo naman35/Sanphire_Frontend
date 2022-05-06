e0eb701cf6ca5f52b724696ec4aea24e23695836
Naman Agarwal <agarwalnaman35@gmail.com>
2022-05-06 23:03:44 +0530

    Speech recognition

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -17,6 +17,9 @@ export default function ChatInput({ handleSendMsg }) {
 
 
 
+  const { transcript, listening, browserSupportsSpeechRecognition } = useSpeechRecognition();
+
+  const { speak } = useSpeechSynthesis();
 
   return (
     <Container>
