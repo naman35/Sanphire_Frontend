46b23224b1c6828c1b7a6f6e1846b19d2fb7a6b3
Naman Agarwal <agarwalnaman35@gmail.com>
2022-05-10 21:25:19 +0530

    Sentry Logging

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import "./index.css";
+
+Sentry.init({
+    dsn: "https://3cd95caa40e043cb96c8d75bd0b1c584@o1240632.ingest.sentry.io/6392892",
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
